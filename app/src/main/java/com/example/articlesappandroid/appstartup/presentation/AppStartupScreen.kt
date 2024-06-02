d08a90c24a9b8c1059af1d051e950cbbc815a85f
JBTG <pochopienantek@gmail.com>
2024-06-02 20:11:05 +0200

    handle app startup states

--- app/src/main/java/com/example/articlesappandroid/appstartup/presentation/AppStartupScreen.kt
+++ app/src/main/java/com/example/articlesappandroid/appstartup/presentation/AppStartupScreen.kt
@@ -1,29 +1,28 @@
 package com.example.articlesappandroid.appstartup.presentation
 
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.articlesappandroid.appstartup.application.AppStartupViewModel
+import com.example.articlesappandroid.appstartup.domain.AppStartupState
 
 
 @Composable
 fun AppStartupScreen(navController: NavController){
-    Greeting(name = "sadsasaddsasdasdadsa sadadsdasdsaads")
+    val viewModel: AppStartupViewModel = viewModel()
+    val state by viewModel.state.collectAsState()
+    when(state){
+        AppStartupState.INITIAL->{
+            //Do nothing
+        }
+        AppStartupState.AUTHORIZED->{
+            //Move to dashboard
+        }
+        AppStartupState.UNAUTHORIZED->{
+            //Move to auth screen
+        }
+    }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    ArticlesAppAndroidTheme {
-//        Greeting("Android")
-//    }
-//}
